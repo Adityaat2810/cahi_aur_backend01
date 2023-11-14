db3f71b485aba84d92fa0f4a0e5fe92bc29edb32
Aditya Kumar Saini <adityasainics10@gmail.com>
2023-11-14 19:11:59 +0530

    utils code disccussion

--- src/db/index.js
+++ src/db/index.js
@@ -9,7 +9,7 @@ const connectDB = async ()=>{
         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
 
         console.log(`\n MongoDB connected !! DB Host: ${connectionInstance.connection.host}`)
-
+    
 
     }catch(error){
         console.log("mongodb connection error ",error)
